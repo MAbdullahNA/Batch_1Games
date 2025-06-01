49715ecca33ac373f44e21b6355aa215678f92d5
Hussain Tech Labz <102964719+MAbdullahNA@users.noreply.github.com>
2025-06-01 13:26:14 +0500

    clamp player horizontal position

--- Assets/3D_RunnerGame/Scripts/PlayerController.cs
+++ Assets/3D_RunnerGame/Scripts/PlayerController.cs
@@ -16,6 +16,13 @@ namespace RunnerGame
 
                 transform.Translate(Vector3.right * Time.deltaTime * moveDirection * turnSpeed);
                 transform.Translate(Vector3.forward * Time.deltaTime * forward * forwardSpeed);
+
+                Vector3 playerPos = transform.position;
+
+                float xDir = Mathf.Clamp(playerPos.x, -1.8f, 1.8f);
+                playerPos.x = xDir;
+                transform.position = playerPos;
+                
             }
         }
     }
